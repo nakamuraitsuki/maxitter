77a5bb9c0577d83fb8dc2010d898febf5b862606
nakamura <itsuki1911n@gmail.com>
2023-05-29 18:44:32 +0900

    ユーザーとツイートのテーブルを作成

---
+++ index.js
@@ -0,0 +1,12 @@
+const sqlite3 = require('sqlite3').verbose();
+const queries = require("./queries");
+
+const db = new sqlite3.Database('database.db');
+
+db.serialize(() => {
+    db.run(queries.Tweets.createTable);
+    db.run(queries.Users.createTable);
+});
+
+
+db.close();
